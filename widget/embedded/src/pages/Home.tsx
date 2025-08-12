c0bab14343fd8a5afa525ea5e17a8139ccc10ec0
arlert-armin <arlert@rango.exchange>
2025-08-12 13:01:14 +0000

    fix: test chain

--- widget/embedded/src/pages/Home.tsx
+++ widget/embedded/src/pages/Home.tsx
@@ -298,7 +298,7 @@ export function Home() {
           isVisible={isVisibleExpanded}
         />
       ) : null}
-      <div>Test Mode | Changelog test release</div>
+      <div>Test Mode | Changelog test chain</div>
     </MainContainer>
   );
 }
