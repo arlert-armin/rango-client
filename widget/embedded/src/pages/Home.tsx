acf4e709637405442251661f184de88900adbbdf
arlert-armin <arlert@rango.exchange>
2025-08-12 11:08:25 +0000

    feat: test release

--- widget/embedded/src/pages/Home.tsx
+++ widget/embedded/src/pages/Home.tsx
@@ -298,7 +298,7 @@ export function Home() {
           isVisible={isVisibleExpanded}
         />
       ) : null}
-      <div>Test Mode | Changelog test rabby</div>
+      <div>Test Mode | Changelog test release</div>
     </MainContainer>
   );
 }
