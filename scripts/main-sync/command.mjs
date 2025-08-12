20532dd4f3a5590df6da2845a88e6d726e0ce105
arlert-armin <arlert@rango.exchange>
2025-08-12 11:07:50 +0000

    chore: add release workflow

--- scripts/main-sync/command.mjs
+++ scripts/main-sync/command.mjs
@@ -18,7 +18,10 @@ async function run() {
   console.log(`Result for: pull\n`, pullOutput);
 
   // Merge `next` into `main`
-  const mergeOutput = await merge('next',{ mergeStrategy: '--no-ff' });
+  const mergeOutput = await merge('next', {
+    mergeStrategy: '--no-ff',
+    messages: ['chore: sync next with main', '[skip ci]'],
+  });
   console.log(`Result for: merge\n`, mergeOutput);
 
   // Push merged commits to `main`
