c3d3c523ec163bf9ca156608a3770bae341140b9
arlert-armin <arlert@rango.exchange>
2025-08-12 07:17:39 +0000

    feat: fix the versions

--- widget/embedded/src/pages/Home.tsx
+++ widget/embedded/src/pages/Home.tsx
@@ -298,7 +298,7 @@ export function Home() {
           isVisible={isVisibleExpanded}
         />
       ) : null}
-      <div>Test Mode | Changelog with chunk fix</div>
+      <div>Test Mode | Changelog with better version</div>
     </MainContainer>
   );
 }
