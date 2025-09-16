3efcbfe916e11241ca67038c0e4d1fa07248ee2d
arlert-armin <arlert@rango.exchange>
2025-09-16 09:54:16 +0000

    fix: remove extra subject

--- scripts/common/constants.mjs
+++ scripts/common/constants.mjs
@@ -1,6 +1,5 @@
 /** A fixed subject for using in publish commit. */
 export const PUBLISH_COMMIT_SUBJECT = 'chore(release): publish';
-export const ROOT_VERSIONS_COMMIT_SUBJECT = 'chore(release): root versions and changelog';
 export const NPM_ORG_NAME = '@arlert-dev';
 export const BUILD_META_FILE_SUFFIX = '.build.json';
 export const ROOT_VERSIONS_COMMIT_SUBJECT =
