1f7328295ce028ddff8974a10981a54db3de8b71
arlert-armin <arlert@rango.exchange>
2025-09-17 07:11:56 +0000

    fix: make it plural

--- widget/embedded/src/pages/Home.tsx
+++ widget/embedded/src/pages/Home.tsx
@@ -298,7 +298,7 @@ export function Home() {
           isVisible={isVisibleExpanded}
         />
       ) : null}
-      <div>Test Mode | Changelog test with missing path</div>
+      <div>Test Mode | Changelog test with missing paths</div>
     </MainContainer>
   );
 }
