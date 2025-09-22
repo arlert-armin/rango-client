fc35895f1fa447db82b67103108ce894ad50f323
arlert-armin <arlert@rango.exchange>
2025-09-22 12:58:45 +0000

    feat: check ternary

--- widget/embedded/src/pages/Home.tsx
+++ widget/embedded/src/pages/Home.tsx
@@ -298,7 +298,7 @@ export function Home() {
           isVisible={isVisibleExpanded}
         />
       ) : null}
-      <div>Test Mode | Changelog test with missing next</div>
+      <div>Test Mode | Changelog test with nonworking ternary</div>
     </MainContainer>
   );
 }
